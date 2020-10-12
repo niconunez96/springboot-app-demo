c8347258a78b4b9da927880836d8323177c49b81
niconunez96 <nicolas110996@gmail.com>
2020-10-12 09:34:48 -0300

    Add springboot project

---
+++ demodeploy/src/test/java/com/taller/tpdeploy/DemoDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.tpdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
